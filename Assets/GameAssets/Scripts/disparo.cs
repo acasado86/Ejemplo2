b1054e2a590bc939dca7d38407b295d5207ff979
Antonio Casado <cice@AULA1POVM12.local>
2014-11-29 11:26:19 +0100

    Disparo bola desde github

---
+++ Assets/GameAssets/Scripts/disparo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disparo : MonoBehaviour {
+	
+	public Transform proyectilPrefab;
+	public Transform posicionDisparo;
+	public float fuerzaDisparo=10f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( (Input.GetKeyDown(KeyCode.LeftControl) ) || (Input.GetMouseButtonDown(0))) {
+			Transform nuevoProyectil;
+			nuevoProyectil = (Transform) Instantiate(proyectilPrefab);
+			nuevoProyectil.transform.position = this.posicionDisparo.transform.position;
+			nuevoProyectil.rigidbody.AddForce(Camera.main.transform.forward*fuerzaDisparo, ForceMode.Impulse);
+		}
+	}
+}
